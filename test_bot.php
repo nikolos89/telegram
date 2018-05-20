ac86310f9bc98cf167891fc425880177c05dc69f
nikolos89 <39451364+nikolos89@users.noreply.github.com>
2018-05-20 11:48:12 +0600

    Update test_bot.php

--- test_bot.php
+++ test_bot.php
@@ -13,19 +13,19 @@ class TestBot extends TelegramBot{
 	 * Fill token after setting webhook
 	 * 
 	 */
-	//protected $token = "";
+	protected $token = "598570912:AAH-WFkt5H5ueSkqahGMT6EHXLmZpqoZakA";
 
 	/**
 	 * Fill you bot name if you want to use it in groups
 	 * @example "@my_test_bot"
 	 */
-	//protected $bot_name = "@";
+	protected $bot_name = "@pogoda55_omsk_bot";
 
 	/**
 	 * HTTP proxy URI (not socks)
 	 * @example "tcp://122.183.137.190:8080"
 	 */
-	//public $proxy = "tcp://122.183.137.190:8080";
+	public $proxy = "tcp://95.110.224.30:18332";
 
 
 }
