692cd3259a0508414271e03452d831d6c6e4b6ab
nikolos89 <39451364+nikolos89@users.noreply.github.com>
2018-05-20 11:21:45 +0600

    Update function.php

--- function.php
+++ function.php
@@ -11,4 +11,33 @@ function teleToLog($log) {
   fclose($fh);
 }
 
+function getUserRequest($text, $chat_id) {
+  //список приветствий, который понимает бот
+  $hello = array();
+  $hello[] = 'привет';
+  $hello[] = 'хай';
+  $hello[] = 'здорова';
+  $hello[] = 'здравствуйте';
+  $hello[] = 'здрасьте';
+  $hello[] = 'йо';
+
+  // варианты ответов бота
+  $bot_hello = array();
+  $bot_hello[] = 'И тебе привет';
+  $bot_hello[] = 'Привет от голоса';
+  $bot_hello[] = 'Доброго времени суток';
+  $bot_hello[] = 'Привет привет';
+
+  if (in_array(mb_strtolower($text), $hello)) {
+    //пользователь поздоровался.
+    //случайная фраза привет от бота
+    $bot_resp = $bot_hello[rand(0, (count($bot_hello) - 1))];
+    $data = array(
+      'text' => $bot_resp,
+      'chat_id' => $chat_id,
+    );
+    requestToTelegram($data);
+  }
+}
+
 ?>
