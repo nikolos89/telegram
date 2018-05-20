d4d73b50a57c22f9d1431cf115bc9ecd19bd0306
nikolos89 <39451364+nikolos89@users.noreply.github.com>
2018-05-20 11:27:47 +0600

    Update index.php

--- index.php
+++ index.php
@@ -10,22 +10,6 @@
 define('BOT_TOKEN', '598570912:AAH-WFkt5H5ueSkqahGMT6EHXLmZpqoZakA');
 define('API_URL', 'https://api.telegram.org/bot' . BOT_TOKEN . '/');
 
-include_once './functions.php';
-
-//принимаем запрос от бота(то что напишет в чате пользователь)
-$content = file_get_contents('php://input');
-//превращаем из json в массив
-$update = json_decode($content, TRUE);
-//получаем id чата
-$chat_id = $update['message']['chat']['id'];
-
-//получаем текст запроса
-$text = $update['message']['text'];
-
-//запись в лог
-teleToLog($update);
-
-//обработка запроса
-getUserRequest($text, $chat_id);
+phpinfo();
 
 ?>
