473f09ddf950c2720293da9b4a8aad67d016262b
nikolos89 <39451364+nikolos89@users.noreply.github.com>
2018-05-20 11:23:14 +0600

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,13 @@ $update = json_decode($content, TRUE);
 //получаем id чата
 $chat_id = $update['message']['chat']['id'];
 
+//получаем текст запроса
+$text = $update['message']['text'];
+
 //запись в лог
 teleToLog($update);
+
+//обработка запроса
+getUserRequest($text, $chat_id);
 
 ?>
