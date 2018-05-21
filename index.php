1ee2f91471aec5a717975a4eb6cd47cb6a45234e
nikolos89 <39451364+nikolos89@users.noreply.github.com>
2018-05-21 09:33:45 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,79 +1,32 @@
-<HTML>
-<HEAD>
-<TITLE>Simple Calculator by Peter Rekdal Sunde</TITLE>
-</HEAD>
-<BODY bgcolor="#57738E">
-
-
-<p>&nbsp;</p>
-
-
-<p>&nbsp;</p>
-
-
-
-<p>&nbsp;</p>
-
-
-
-<p>&nbsp;</p>
-
-
-
-<p>&nbsp;</p>
-
-
-
-<p>&nbsp;</p>
-
-
-
-<p>&nbsp;</p>
-
-
-
-<p>&nbsp;</p>
-<div align="center">
-  <center>
-  <table border="0" width="966" height="203">
-    <tr>
-      <td width="966" height="1" bgcolor="#FFFFFF" align="center">
-        <p align="center"><font face="Verdana" size="1"><b>
-        <marquee>Download this code at <a href="http://www.planet-source-code.com">http://www.planet-source-code.com</a></marquee>
-        </b></font></td>
-    </tr>
-    <tr>
-      <td width="966" height="241" align="center">
-
-
-
-<FORM METHOD="post" ACTION="result.php">
-
-<P><font face="Verdana" size="2"><b>Calculation number 1: <INPUT TYPE="text" NAME="val1" SIZE=10></b></font></p>
-
-<P><font face="Verdana" size="2"><b>Calculation number 2: <INPUT TYPE="text" NAME="val2" SIZE=10></b></font></p>
-
-<p><font face="Verdana" size="2"><b>Calculation type:</b></font></p>
-
-<p><font face="Verdana" size="2"><b><input TYPE="radio" NAME="calc" VALUE="addisjon">
-add <input TYPE="radio" NAME="calc" VALUE="minus"> subtract <input TYPE="radio" NAME="calc" VALUE="ganger">
-multiply <input TYPE="radio" NAME="calc" VALUE="dele"> divide</b></font></p>
-
-<P><INPUT TYPE="submit" NAME="submit" VALUE="Do Calculation"></p>
-
-</FORM>
-   
-
-      </td>
-    </tr>
-    <tr>
-      <td width="966" height="1" bgcolor="#FFFFFF">
-        <p align="center"><font face="Verdana" size="1"><b>This script was
-        created by Peter Rekdal Sunde</b></font></td>
-    </tr>
-  </table>
-  </center>
-</div>
-
-</BODY>
-</HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251"> 
+<title>Калькулятор</title>
+</head>
+<body>
+<?             /* приступаем непосредственно php коду */
+function show()
+{
+  global $action;
+  ?>
+  <FORM method=Get action="calc.php" target="_blank">
+/* target="_blank" - результат обработки формы будет выводится в новом окне */
+  Первое число
+  <input type="text" name="first">
+  Второе число (Степень, процент)
+  <input type="text" name="second">
+  <select size="1" name="action">
+    <option value="sum">Сложить</option>
+    <option value="min">Вычесть</option>
+    <option value="mult">Умножить</option>
+    <option value="dev">Разделить</option>
+    <option value="stepen">Возвести в степень</option>
+    <option value="procent">Процент от числа</option>
+    <option value="koren">Корень</option>
+  </select>
+  <br><input type="submit" value="Выполнить">
+  </form>
+  <?php 
+}       //конец функции show()
+</body>
+  </html>
