bc2c2f68c61637e35830a6f012085516f398dca5
nikolos89 <39451364+nikolos89@users.noreply.github.com>
2018-05-21 09:26:50 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+ 
 // Функция вывода формы ввода
 function show_form()
 {
@@ -7,7 +8,7 @@ echo '<head>';
 echo '<title>Калькулятор</title>';
 echo '</head>';
 echo '<body>';
-echo '<form action="sum1.php" method="post">';
+echo '<form action="#" method="post">';
 echo '<input type="text" name="a"/>';
  
 echo '<input type="submit" name="oper" value= "+" />';
@@ -22,56 +23,32 @@ echo '</form>';
 echo '</body>';
 echo '</html>';
 }
-// Функция вычисления результата.
-//function count_result($oper,$a, $b)
-//{
-$oper = $_POST['oper'];
-if ($oper == "+")
-{
-$result = $a + $b;
  
-}
-else if ($oper == "-")
-{
-$result = $a - $b;
  
-}
-else if ($oper == "*")
-{
-$result = $a * $b;
  
-}
-else if ($oper == "/")
-{
-if ($b != 0)
-{
-$result = $a / $b;
- 
-}
-else
-{ 
-echo "на ноль делить нельзя!";
-}
-}
- 
-//}
  
 // Функция вывода результата.
-function show_result($a, $b)
+function show_result($a, $b , $oper) 
 {
+ 
+ $result = null;
+ if ($oper == "+")       {  $result = $a + $b;  }
+ else if ($oper == "-")  {  $result = $a - $b;  }
+ else if ($oper == "*")  {  $result = $a * $b;  }
+ else if ($oper == "/")  {  $result = $b != 0 ? $a / $b : "на ноль делить нельзя!"; }
 echo '<html>';
 echo '<head>';
 echo '<title>Сумматор</title>';
 echo '</head>';
 echo '<body>';
-echo '<form action="sum1.php" method="get">';
-echo '<input type = "text" name = "a" value = '.$_POST['a'].'>';
-echo '<input type="submit" value="+" />';
-echo '<input type="submit" value="-" />';
-echo '<input type="submit" value="*" />';
-echo '<input type="submit" value="/" />';
+echo '<form action="#" method="get">';
+echo '<input type = "text" name = "a" value = '.$a.'>';
+echo '<input type="submit" name="oper" value= "+" />';
+echo '<input type="submit" name="oper" value= "-" />';
+echo '<input type="submit" name="oper" value= "*" />';
+echo '<input type="submit" name="oper" value= "/" />';
  
-echo '<input type = "text" name = "b" value = '.$_POST['b'].'>';
+echo '<input type = "text" name = "b" value = '.$b.'>';
 echo '<input type="submit" value="=" />';
 echo $result;
 echo '<br>';
@@ -83,13 +60,12 @@ echo '</html>';
  
 // Точка входа.
 // Показываем результат операции или форму ввода.
-if (isset($_POST['a']) && isset($_POST['b']))
+if (!empty($_POST['oper']) && !empty($_POST['a']) && !empty($_POST['b']))
 {
-show_result($_POST['a'], $_POST['b'],$oper);
+show_result($_POST['a'], $_POST['b'],$_POST['oper']);
 }
 else
 {
 show_form();
 }
- 
 ?>
