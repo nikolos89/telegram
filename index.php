de0cd293c65bead2c82b08ac88b8a66ec84aa86d
nikolos89 <39451364+nikolos89@users.noreply.github.com>
2018-05-21 11:18:45 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
-echo '
-<div id="time">
-        <?php echo date('H:i:s d.m.Y');?>
-    </div>
-    <script>
-        let time = new Date(document.getElementById('time').innerText);
-    </script>
-';
+/* Установка голландской локали */
+setlocale(LC_ALL, 'nld_nld');
+
+/* выводит: vrijdag 22 december 1978 */
+echo strftime("%A %d %B %Y", mktime(0, 0, 0, 12, 22, 1978));
+
+/* попытка использовать различные локали для немецкого языка */
+$loc_de = setlocale(LC_ALL, 'de_DE@euro', 'de_DE', 'deu_deu');
+echo "Предпочитаемая немецкая локаль на этой системе: '$loc_de'";
 echo '<br/>';
 echo getenv('REMOTE_ADDR'); 
 echo '<br/>';
