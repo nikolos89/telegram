b5ed25fcb7d7993114905f5fe88494a4784ea088
nikolos89 <39451364+nikolos89@users.noreply.github.com>
2018-05-21 10:17:28 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 echo rand(1,1000000000); 
+
+echo date('H:i:s d.m.Y');
+
+echo getenv('REMOTE_ADDR'); 
+
+define('ST_T', microtime());//начало
+
+echo '<p>Контент страницы, скрипты и т.п.</p>';
+
+printf('Страница сгенерирована за %.5f сек.', microtime()-ST_T);//конец
+
+
 ?>
