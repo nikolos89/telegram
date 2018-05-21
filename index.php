dfbdd027cda5fdb7e2d3d4bcd8452b4ee1f854fc
nikolos89 <39451364+nikolos89@users.noreply.github.com>
2018-05-21 10:14:22 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,3 @@
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1251"> 
-<title>Калькулятор</title>
-</head>
-<body>
-  <?php            
-function show()
-{
-  global $action;
-  ?>
-  <FORM method=Get action="calc.php" target="_blank">
-
-  <input type="text" name="first">
-
-  <input type="text" name="second">
-  <select size="1" name="action">
-    <option value="sum">Сложить</option>
-    <option value="min">Вычесть</option>
-    <option value="mult">Умножить</option>
-    <option value="dev">Разделить</option>
-    <option value="stepen">Возвести в степень</option>
-    <option value="procent">Процент от числа</option>
-    <option value="koren">Корень</option>
-  </select>
-  <br><input type="submit" value="Выполнить">
-  </form>
-  } 
+<?php
+echo file_get_contents('http://www.dogpile.com/');
 ?>
-    
-</body>
-  </html>
